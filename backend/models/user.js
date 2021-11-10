117dbc2d5d4ca1f34b3fd8cfab487a262a002509
Szymon Żur <szymon.zur2@gmail.com>
2021-11-10 22:37:01 +0100

    Create initial schema models

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+  country: {
+    type: String,
+    default: "",
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
 
-})
-
-exports.User = mongoose.model('User', userSchema);
+exports.User = mongoose.model("User", userSchema);
