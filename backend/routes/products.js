e51c8d1205d5f2f862a2de1a05beb1d65ce9de24
Szymon Żur <szymon.zur2@gmail.com>
2021-11-11 20:47:04 +0100

    create post method for adding products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -11,4 +11,20 @@ router.get(`/`, async (req, res) =>{
     res.send(productList);
 })
 
+router.post("/", async (req, res) => {
+    let newProduct = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+    });
+    newProduct = await newProduct.save();
+  
+    if (!newProduct) {
+      res.status(400).send("The favorite product cannot be added!");
+    }
+    res.send(newProduct);
+  });
+
 module.exports =router;
