d7432b7ae404489a7e064d2bd13a020ea7053aca
Szymon Żur <szymon.zur2@gmail.com>
2021-12-01 16:24:47 +0100

    implement redux

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ShopNavigator from "./navigation/ShopNavigator";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 export default function App() {
-  return <ShopNavigator />;
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  );
 }
